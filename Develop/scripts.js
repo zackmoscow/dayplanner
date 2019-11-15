6953cf692cf682a31c30a295f6add96c7994b0fb
Zack Moscow <zmoscow@gmail.com>
2019-11-14 21:28:49 -0600

    nearly complete, having issue with fontawesome logo

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -1,17 +1,68 @@
-var currentDay = moment().format("dddd, MMMM Do, YYYY");
-    $("#currentDay").text(currentDay);
+$(document).ready(function() {
 
-var colorAssign = $(".col-8");
-var currentTime = moment().hour();
-    if (parseInt(colorAssign.attr("data-moment")) > currentTime) {
-        colorAssign.attr("class", "col-8 future");
-    } 
-    if (parseInt(colorAssign.attr("data-moment")) < currentTime) {
-        colorAssign.attr("class", "col-8 past");
-    } 
-    if (parseInt(colorAssign.attr("data-moment")) === currentTime) {
-        colorAssign.attr("class", "col-8 present");
-    } 
+    //SET ALL NECESSARY TIME VARIABLES VIA MOMENT.JS
 
-var saveBtn = $(".saveBtn");
+    var currentDay = moment().format("dddd, MMMM Do, YYYY");
+        $("#currentDay").text(currentDay);
 
+    var currentTime = moment().hour();
+    var currentTimeNum = Number(currentTime);
+
+    //CREATE PLANNER BODY WITH NECESSARY CLASS AND ID ATTRIBUTES
+
+    for (i = 9; i < 18; i++) {
+        var rowDiv = $("<div class='row dateRow'>");
+        var leftCol = $("<div class='col-2 hour'>");
+        var centerCol = $("<div class='col-8 activity'>");
+            centerCol.attr("id", i);
+        var rightCol = $("<div class='col-2 saveBtn'>");
+            rightCol.attr("id", i)
+        var hourOfDay = $("<p>").text(i + ":00");
+        var taskInput = $("<textarea>");
+            taskInput.attr("class", i)
+        var saveButton = $("<span class'fas fa-save fa-2x'>");
+        rowDiv.append(leftCol);
+        rowDiv.append(centerCol);
+        rowDiv.append(rightCol);
+        rightCol.append(saveButton);
+        centerCol.append(taskInput);
+        leftCol.append(hourOfDay);
+        $(".container").append(rowDiv);
+    };
+        
+    //ASSIGN STYLE CLASSES TO HOUR ROWS BASED ON IDS SET ABOVE
+
+    for (i = 9; i < 18; i++) {  
+        var idCheck = '#' + i;
+        if (i > currentTimeNum) {
+            $(idCheck).addClass("future");
+        };
+        if (i === currentTimeNum) {
+            $(idCheck).addClass("present");
+        };
+        if (i < currentTimeNum) {
+            $(idCheck).addClass("past"); 
+        };      
+    };
+
+    //LOAD LOCAL STORAGE IF IT EXISTS
+
+    var activities = JSON.parse(localStorage.getItem('planner')) || {};
+
+    function loadActivities() {
+        for (i = 9; i < 18; i++) {
+            $(`.${i}`).text(activities[i]);
+        }
+    }
+
+    //SAVE USER INPUT TO LOCAL STORAGE
+    $(".saveBtn").on('click', function() {
+        var activityTime = $(this).attr("id");
+        var activityDetail = $(this).prev().find("textarea").val();
+        activities[activityTime] = activityDetail;
+        localStorage.setItem('planner', JSON.stringify(activities));
+    });
+
+    loadActivities();
+
+});
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # dayplanner
+
+## INDEX.HTML
+
+- added all necessary style/script links, including bootstrap, moment.js, jquery, fontawesome
+- otherwise all content generated dynamically
+
+## STYLE.CSS
+
+- slight modification for save button hovering but otherwise untouched
+
+## SCRIPTS.JS
+
+### TIME VARIABLES
+
+- set currentDay variable to pull moment.js data and populate into jumbotron
+- get current time (hour only) and convert string to number
+
+### PLANNER BODY
+
+- originally built this in static html and then "translated" into JS
+- note "i" id assignments for use later in the application
+
+### ASSIGN STYLE CLASSES TO ROWS
+
+- this should be pretty self-explanatory
+
+### LOAD LOCAL STORAGE
+
+- here's where this got complicated for me...
+- figured out that i needed to let activities array equal saved values OR that it needed to be empty
+- getting local storage to populate into the correct textarea was very challenging. after a lot of research,  it was clear that using a template literal would be the easiest solution. i had to look at some examples    to get it right...
+
+### SAVE TO LOCAL STORAGE
+
+- added on(click) functionality to saveBtn class
+- activityTime was easy
+- activityDetail was tougher. researched .prev() jQuery function and then had to play with .find() to make    sure the text area was targeted (this took a lot of trial and error). 
+- make sure to use .val() as it is targeting an input
+- stringify final result in preparation for local storage
